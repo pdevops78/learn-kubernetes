ba58ee079c1cf2e107c47ef97fd9ee82dec65992
Your Name <vemasani.pavani@gmail.com>
2025-08-13 11:20:13 +0530

    loop

--- content/image/run.sh
+++ content/image/run.sh
@@ -1,2 +1,9 @@
 mkdir -p /usr/share/nginx/html
-echo Hello >/usr/share/nginx/html/index.html
+while true ; do
+  echo "<h1>Hello-${date}" >/usr/share/nginx/html/index.html
+  sleep 1
+done
+# in this code container will be in running state for every 1 second and loop not to terminate until manually stops
+# if we not mention sleep 1 then container will be in exited status.
+
+#echo Hello >/usr/share/nginx/html/index.html
