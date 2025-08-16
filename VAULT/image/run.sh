483b798d0c18a6ba849b688bbc4ea18ee0129ee2
Your Name <vemasani.pavani@gmail.com>
2025-08-17 00:51:23 +0530

    vault image

--- VAULT/image/run.sh
+++ VAULT/image/run.sh
@@ -1,4 +1,4 @@
 vault login $VAULT_TOKEN
- vault kv get $SECRET_NAME |sed -n -e '/= Data =/,$p' | grep -Ev '= Data =|^key|^--'| awk '{print "export "$1"="$2}' >/tmp/data
- cat /tmp/data
+ vault kv get $SECRET_NAME |sed -n -e '/= Data =/,$p' | grep -Ev '= Data =|^Key|^--'| awk '{print "export "$1"="$2}' >/tmp/data.txt
+ cat /tmp/data.txt
 
