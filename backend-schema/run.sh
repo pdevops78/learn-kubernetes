4af0ecd5ec130dc770cb5489747c973f595a8db6
Your Name <vemasani.pavani@gmail.com>
2025-08-17 00:11:16 +0530

    vault image

--- backend-schema/run.sh
+++ backend-schema/run.sh
@@ -1,20 +1,20 @@
-source /data/ex.sh
+source /data/secrets.txt
 
-if [ -z "$DB_HOST" ]; then
+if [ -z "${DB_HOST}" ]; then
   echo DB_HOST is missing
   exit 1
 fi
 
-if [ -z "$NEW_RELIC_APP_NAME" ]; then
+if [ -z "${NEW_RELIC_APP_NAME}" ]; then
   echo NEW_RELIC_APP_NAME is missing
   exit 1
 fi
 
-if [ -z  "$NEW_RELIC_LICENSE_KEY"]; then
+if [ -z  "${NEW_RELIC_LICENSE_KEY}" ]; then
     echo NEW_RELIC_LICENSE_KEY is missing
     exit 1
 fi
 
 git clone https://github.com/pdevops78/expense-backend
 cd expense-backend
-mysql -h${DB_HOST} -u{rds_name} -p{rds_password} < /app/schema/backend.sql
+mysql -h${DB_HOST} -u${rds_name} -p${rds_password} < /app/schema/backend.sql
