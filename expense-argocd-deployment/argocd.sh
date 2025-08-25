90143a89f42c8f0d4a4f0f0571a980b14e068093
Your Name <vemasani.pavani@gmail.com>
2025-08-25 07:55:21 +0530

    eks

--- expense-argocd-deployment/argocd.sh
+++ expense-argocd-deployment/argocd.sh
@@ -1,10 +1,24 @@
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-echo ${kubectl get svc argocd-server  -n argocd | awk '{print $4}' | tail -1}
-echo ${argocd admin initial-password -n argocd}
+# to install argocd
+if [ $1 == "install" ]; then
+  kubectl create namespace argocd
+  kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+  kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+  sleep 2
+  echo ${kubectl get svc argocd-server  -n argocd | awk '{print $4}' | tail -1}
+  echo admin
+  echo ${argocd admin initial-password -n argocd} | head -1
 
-#kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
-sleep 2
+  #kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
+fi
+#  to add jobs in argocd
+  if [ $1 == "jobs" ]; then
+   argocd login ${kubectl get svc argocd-server  -n argocd | awk '{print $4}' | tail -1} --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
+   for app in frontend backend; do
+   argocd app create ${app} --repo https://github.com/pdevops78/expense-helm-argocd.git --path chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default --insecure  --grpc-web --values values/${app}.yaml
+  done
+fi
 
-argocd app create backend --repo https://github.com/pdevops78/learn-kubernetes.git --path expense-chart/chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default.svc --insecure --skip-test-tls --grpc-web --values expense-chart/values/backend.yaml
+# to override values.yaml file in nginx ingress
+helm upgrade nginx-ingress ingress-nginx/ingress-nginx --namespace ingress-nginx -f values.yaml
+
+
