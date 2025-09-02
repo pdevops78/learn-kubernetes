1c6ee3401e164ce730a363e3fc6e14fb96f93025
Your Name <vemasani.pavani@gmail.com>
2025-09-02 14:23:46 +0530

    eks

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
-provider "vault" {
-  address = "https://vault-internal.pdevops78.online:8200"
-  skip_tls_verify = true
-  token = var.vault_token
-}
+# provider "vault" {
+#   address = "https://vault-internal.pdevops78.online:8200"
+#   skip_tls_verify = true
+#   token = var.vault_token
+# }
 
 provider "kubernetes" {
   config_path = "~/.kube/config"
