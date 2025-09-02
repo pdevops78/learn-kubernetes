7eb733b6177b4abe38a054afbff57407be381cd7
Your Name <vemasani.pavani@gmail.com>
2025-09-02 16:33:27 +0530

    eks

--- module/eks/main.tf
+++ module/eks/main.tf
@@ -97,7 +97,7 @@ resource "aws_eks_pod_identity_association" "external--pod-association" {
   cluster_name    = aws_eks_cluster.cluster.name
   namespace       = "default"
   service_account = "external-dns"
-  role_arn        = aws_iam_role.external_dns.arn
+  role_arn        = aws_iam_role.external-dns.arn
 }
 
 
