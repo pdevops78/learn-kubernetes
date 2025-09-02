a8e5c0fdb24ba5ba580dbbd165274c624f4c1db7
Your Name <vemasani.pavani@gmail.com>
2025-09-02 14:32:41 +0530

    eks

--- module/eks/main.tf
+++ module/eks/main.tf
@@ -88,11 +88,12 @@ resource "helm_release" "external_dns" {
   namespace  = "kube-system"
   repository = "https://kubernetes-sigs.github.io/external-dns/"
   chart      = "external-dns"
-  set {
-    name  = "serviceAccount.name"
-    value = "dns-sa"
-
-  }
+  set = [
+    {
+      name  = "serviceAccount.name"
+      value = "dns-sa"
+    }
+  ]
 }
 
 
