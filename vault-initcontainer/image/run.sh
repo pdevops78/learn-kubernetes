87220f94ffa5e960f29994d699ca868f8bafb153
Your Name <vemasani.pavani@gmail.com>
2025-09-06 11:34:26 +0530

    backend schema

--- vault-initcontainer/image/run.sh
+++ vault-initcontainer/image/run.sh
@@ -1,5 +1,5 @@
 vault login $VAULT_TOKEN
- vault kv get $SECRET_NAME |sed -n -e '/= Data =/,$p' | grep -Ev '= Data =|^Key|^--'| awk '{print "export "$1"="$2}' >/tmp/data.txt
- cat /tmp/data.txt
+ vault kv get $SECRET_NAME |sed -n -e '/= Data =/,$p' | grep -Ev '= Data =|^Key|^--'| awk '{print "export "$1"="$2}' >/tmp/secrets.txt
+ cat /tmp/secrets.txt
 
 # vault kv get common/common | sed -n -e '/= Data =/,$p' | grep -Ev '= Data =|^key|^---'| awk '{print "export "$1"="$2}'
