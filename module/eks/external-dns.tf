665d1abcd338197a95638eb342b49b2e30672a06
Your Name <vemasani.pavani@gmail.com>
2025-09-12 22:31:56 +0530

    external dns

--- module/eks/external-dns.tf
+++ module/eks/external-dns.tf
@@ -12,18 +12,18 @@
 #   }
 # }
 # #  create pod identity and  attach to cluster
-# resource "aws_eks_pod_identity_association" "external--pod-association" {
-#   cluster_name    = aws_eks_cluster.cluster.name
-#   namespace       = "default"
-#   service_account = "dns-sa"
-#   role_arn        = aws_iam_role.external-dns.arn
-# }
-#
-# resource "aws_eks_addon" "eks-pod-identity-agent" {
-#   depends_on                  = [aws_eks_node_group.node]
-#   cluster_name                = aws_eks_cluster.cluster.name
-#   addon_name                  = "eks-pod-identity-agent"
-#   addon_version               = "v1.3.2-eksbuild.2"
-#   resolve_conflicts_on_update = "OVERWRITE"
-#   resolve_conflicts_on_create = "OVERWRITE"
-# }
+resource "aws_eks_pod_identity_association" "external--pod-association" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  namespace       = "default"
+  service_account = "dns-sa"
+  role_arn        = aws_iam_role.external-dns.arn
+}
+
+resource "aws_eks_addon" "eks-pod-identity-agent" {
+  depends_on                  = [aws_eks_node_group.node]
+  cluster_name                = aws_eks_cluster.cluster.name
+  addon_name                  = "eks-pod-identity-agent"
+  addon_version               = "v1.3.2-eksbuild.2"
+  resolve_conflicts_on_update = "OVERWRITE"
+  resolve_conflicts_on_create = "OVERWRITE"
+}
