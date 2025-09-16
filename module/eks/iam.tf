69ba4356896edd8660ca455e66d738da5b99bde1
Your Name <vemasani.pavani@gmail.com>
2025-09-16 12:03:53 +0530

    ebs csi driver

--- module/eks/iam.tf
+++ module/eks/iam.tf
@@ -52,8 +52,5 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.node-role.name
 }
-resource "aws_iam_role_policy_attachment" "AmazonEBSCSIDriverPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-  role       = aws_iam_role.node-role.name
-}
+
 
