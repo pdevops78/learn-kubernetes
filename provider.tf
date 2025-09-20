1ab987aa1503a3d95a8b7b7395752caa18625dbb
Your Name <vemasani.pavani@gmail.com>
2025-09-20 10:18:54 +0530

    create pvc for to create ebs volume

--- provider.tf
+++ provider.tf
@@ -7,16 +7,19 @@
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
-#
-# terraform {
-#   required_providers {
-#     helm = {
-#       source  = "hashicorp/helm"
-#       version = "2.11.0"
-#     }
-#   }
-# }
-#
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.8.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.11.0"
+    }
+  }
+}
+
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
