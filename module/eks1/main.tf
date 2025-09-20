35f6ea7a336d11a71cf84d1f03e54af5d35346b6
Your Name <vemasani.pavani@gmail.com>
2025-09-20 09:04:38 +0530

    comment vpc code for a while

--- module/eks1/main.tf
+++ module/eks1/main.tf
@@ -30,7 +30,7 @@ resource "aws_eks_node_group" "node" {
 //arn:aws:iam::041445559784:role/eks-cluster-example
 
 resource "aws_launch_template" "main" {
-  name = "eks-${var.env}"
+  name        = "eks-${var.env}"
   device_name = "/dev/xvda"
 
   #   ebs {
@@ -39,14 +39,13 @@ resource "aws_launch_template" "main" {
   #     kms_key_id            = var.kms_key_id
   #     delete_on_termination = true
   #   }
-}
-tag_specifications {
-  resource_type = "instance"
-  tags = {
-    "Name" = "${aws_eks_cluster.cluster.name}-workernode"
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      "Name" = "${aws_eks_cluster.cluster.name}-workernode"
+    }
   }
 }
-
 
 resource "null_resource" "aws-auth" {
   depends_on = [aws_eks_node_group.node]
