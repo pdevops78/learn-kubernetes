3fc19833b872583c91b3cca77017f81745d4ea22
Your Name <vemasani.pavani@gmail.com>
2025-09-23 05:00:06 +0530

    kibana

--- module/eks1/autoscaling.tf
+++ module/eks1/autoscaling.tf
@@ -1,23 +1,23 @@
-# resource "aws_iam_role" "autoscaling_role" {
-#   name               = "autoscaling-role"
-#   assume_role_policy = data.aws_iam_policy_document.policy_role.json
-# }
-#
-# resource "aws_iam_role_policy" "autoscaling_policy" {
-#   name = "autoscaling-policy"
-#   role = aws_iam_role.autoscaling_role.id
-#   policy = file("${path.module}/autoscaling-policy.json")
-# }
-#
-# resource "aws_eks_addon" "autoscaling_pod_identity_agent" {
-#   depends_on                  = [aws_eks_node_group.node]
-#   cluster_name                = aws_eks_cluster.cluster.name
-#   service_account_role_arn    = aws_iam_role.autoscaling_role.arn
-#   addon_name                  = "eks-pod-identity-agent"
-#   addon_version               = "v1.3.2-eksbuild.2"
-#   resolve_conflicts_on_update = "OVERWRITE"
-#   resolve_conflicts_on_create = "OVERWRITE"
-# }
+resource "aws_iam_role" "autoscaling_role" {
+  name               = "autoscaling-role"
+  assume_role_policy = data.aws_iam_policy_document.policy_role.json
+}
+
+resource "aws_iam_role_policy" "autoscaling_policy" {
+  name = "autoscaling-policy"
+  role = aws_iam_role.autoscaling_role.id
+  policy = file("${path.module}/autoscaling-policy.json")
+}
+
+resource "aws_eks_addon" "autoscaling_pod_identity_agent" {
+  depends_on                  = [aws_eks_node_group.node]
+  cluster_name                = aws_eks_cluster.cluster.name
+  service_account_role_arn    = aws_iam_role.autoscaling_role.arn
+  addon_name                  = "eks-pod-identity-agent"
+  addon_version               = "v1.3.2-eksbuild.2"
+  resolve_conflicts_on_update = "OVERWRITE"
+  resolve_conflicts_on_create = "OVERWRITE"
+}
 
 resource "helm_release" "autoscaler" {
 
--- module/eks1/ebs-csi.tf
+++ module/eks1/ebs-csi.tf
@@ -10,14 +10,14 @@ resource "aws_iam_role_policy" "ebs_csi_policy" {
 }
 
 resource "aws_eks_addon"  "eks_ebs_csi_driver" {
-#     depends_on                  = [aws_eks_node_group.node]
-#    cluster_name                = aws_eks_cluster.cluster.name
+    depends_on                  = [aws_eks_node_group.node]
+    cluster_name                = aws_eks_cluster.cluster.name
     addon_name                  = "aws-ebs-csi-driver"
     service_account_role_arn    = aws_iam_role.ebs_csi_driver.arn
     addon_version               = "v1.48.0-eksbuild.2"
     resolve_conflicts_on_update = "OVERWRITE"
     resolve_conflicts_on_create = "OVERWRITE"
-    cluster_name                = "eks"
+
 }
 
 
